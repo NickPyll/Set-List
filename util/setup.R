8be9c0e291e3aed672051c05c7ddb7b624c47e87
Nicholas Pylypiw <nick.pylypiw@gmail.com>
2025-01-28 08:26:49 -0500

    subtitle

--- util/setup.R
+++ util/setup.R
@@ -12,14 +12,15 @@ library(janitor)
 
 theme_setlist <-
   theme_wsj() +
-    theme(
-      panel.grid.major.y = element_blank(),
-      panel.grid.major.x = element_line(colour = "grey30", linewidth = 0.2),
-      panel.grid.minor.x = element_blank(),
-      title = element_text(size = 26, color = 'grey30'),
-      text = element_text(color = 'grey40', family = 'Lato'),
-      plot.title.position = "plot",
-      plot.caption = element_text(color = 'grey40', family = 'Lato', size = 14)
+  theme(
+    panel.grid.major.y = element_blank(),
+    panel.grid.major.x = element_line(colour = "grey30", linewidth = 0.2),
+    panel.grid.minor.x = element_blank(),
+    title = element_text(size = 26, color = "grey30"),
+    plot.subtitle = element_text(size = 20),
+    text = element_text(color = "grey40", family = "Lato"),
+    plot.title.position = "plot",
+    plot.caption = element_text(color = "grey40", family = "Lato", size = 14)
   )
 
 theme_set(theme_setlist)
